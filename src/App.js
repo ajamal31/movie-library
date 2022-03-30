24ba2081127f871daf50c9aab9d944642b909b41
ajamal31 <ajamal31@hotmail.com>
2022-03-29 19:24:16 -0600

    Change API to TMDB

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import "./App.css";
 
-const API_URL = "https://www.omdbapi.com?apikey=79f91863";
+// const API_URL = "https://www.omdbapi.com?apikey=79f91863";
+const API_URL = "https://api.themoviedb.org/3/search/multi?api_key=83f43ba8ffc90fdc14a69548089ce760"
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState();
 
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
+        const response = await fetch(`${API_URL}&query=${title}`);
         const data = await response.json();
-        setMovies(data.Search);
+        // console.log(data.results);
+        setMovies(data.results);
     };
 
     useEffect(() => {
-        searchMovies('');
+        searchMovies('superman');
     }, []);
 
     return (
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
 const MovieCard = ({ movie }) => {
+    console.log(movie);
     return (
         <div className="movie">
             <div>
                 <p>{movie.Year}</p>
             </div>
             <div>
-                <img src={movie.Poster} alt={movie.Title} />
+                <img src={`${IMG_URL}${movie.poster_path}`} alt={movie.title} />
             </div>
             <div>
                 <span>{movie.Type}</span>
-                <h3>{movie.Title}</h3>
+                <h3>{movie.title}</h3>
             </div>
         </div>
     );
