0c86df62aa57b005b4ec353df72cd9e7d2435bf9
Arun Jamal <ajamal31@hotmail.com>
2022-03-29 21:12:23 -0600

    Add default results & more attributes on the Movie Card

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 
 // const API_URL = "https://www.omdbapi.com?apikey=79f91863";
-const API_URL = "https://api.themoviedb.org/3/search/multi?api_key=83f43ba8ffc90fdc14a69548089ce760"
+const API_URL =
+    "https://api.themoviedb.org/3/search/multi?api_key=83f43ba8ffc90fdc14a69548089ce760";
+const TOP_MOVIES_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=83f43ba8ffc90fdc14a69548089ce760&language=en-US&page=1";
 
 const App = () => {
     const [movies, setMovies] = useState();
 
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&query=${title}`);
-        const data = await response.json();
-        // console.log(data.results);
-        setMovies(data.results);
+        if (title) {
+            const response = await fetch(`${API_URL}&query=${title}`);
+            const data = await response.json();
+            setMovies(data.results);
+        } else {
+            const response = await fetch(TOP_MOVIES_URL);
+            const data = await response.json();
+            setMovies(data.results);
+        }
     };
 
     useEffect(() => {
-        searchMovies('superman');
+        searchMovies(null);
     }, []);
 
     return (
         <div className="app">
-            <h1>Movie Library</h1>
+            <h1>Television Library</h1>
             <div className="search">
                 <input
                     placeholder="Search for movies"
@@ -30,11 +38,13 @@ const App = () => {
                 />
             </div>
             {movies?.length > 0 ? (
-                <div className="container">
-                    {movies.map((movie) => (
-                        <MovieCard movie={movie}></MovieCard>
-                    ))}
-                </div>
+                <>
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard key={movie.id} movie={movie}></MovieCard>
+                        ))}
+                    </div>
+                </>
             ) : (
                 <div className="empty">
                     <h2>No movies found</h2>
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const DEFAULT_IMG_URL =
+    "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg";
 
 const MovieCard = ({ movie }) => {
-    console.log(movie);
+    let release_date = movie.release_date
+        ? movie.release_date
+        : movie.first_air_date;
+    let title = movie.title ? movie.title : movie.name;
+    let poster = movie.poster_path
+        ? `${IMG_URL}${movie.poster_path}`
+        : DEFAULT_IMG_URL;
+
     return (
         <div className="movie">
             <div>
-                <p>{movie.Year}</p>
+                <h3>{release_date}</h3>
+                <h3>Rating: {movie.vote_average}</h3>
+                <p>{movie.overview}</p>
             </div>
             <div>
-                <img src={`${IMG_URL}${movie.poster_path}`} alt={movie.title} />
+                <img src={poster} alt={movie.title} />
             </div>
             <div>
-                <span>{movie.Type}</span>
-                <h3>{movie.title}</h3>
+                <span>{movie.media_type}</span>
+                <h3>{title}</h3>
             </div>
         </div>
     );
